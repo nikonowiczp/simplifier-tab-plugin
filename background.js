c34f11a06ed45e0d558c3798fcee6d876bf23aef
Patryk Nikonowicz <pk.nikonowicz@gmail.com>
2024-09-19 16:05:20 +0200

    Working v1

--- background.js
+++ background.js
@@ -2,43 +2,116 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete') {
         const url = new URL(tab.url);
         const instance = url.hostname.split('.')[0]; // Extract INSTANCE from hostname
+        const hashParts = url.hash.split('/');
 
         // Check if the URL matches the required structure
         if (url.pathname.includes('/UserInterface/')) {
-            if (url.hash.startsWith('#/BusinessObject/client/')) {
-                const hashParts = url.hash.split('/');
-
-                const boname = hashParts[4];
-                const bofuncname = hashParts[7];
+            if (url.hash.startsWith('#/ClientBusinessObject')) {
+                const sName = hashParts[2];
+                const sFuncName = hashParts[5];
 
 
                 var newTitle = `CSBO `;
-                if (bofuncname) {
-                    newTitle += `${bofuncname}`;
+                if (sFuncName) {
+                    newTitle += `${sFuncName} `;
                 }
-                if (boname) {
-                    newTitle += `|${boname} `;
+                if (sName) {
+                    newTitle += `${sName} `;
                 }
-                newTitle += `|${instance}`;
+                newTitle += `${instance}`;
                 // Send a message to content script to change the title
                 chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
 
-            } else if (url.hash.startsWith('#/BusinessObject/')) {
-                const hashParts = url.hash.split('/');
+            }else if(url.hash.startsWith('#/BusinessObject/client')){
+                const sName = hashParts[3];
+                const sFuncName = hashParts[6];
 
-                const boname = hashParts[3];
-                const bofuncname = hashParts[6];
+
+                var newTitle = `CSBO `;
+                if (sFuncName) {
+                    newTitle += `${sFuncName} `;
+                }
+                if (sName) {
+                    newTitle += `${sName} `;
+                }
+                newTitle += `${instance}`;
+                // Send a message to content script to change the title
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else if (url.hash.startsWith('#/BusinessObject')) {
+                var sName = hashParts[2];
+                if(sName === "server") sName = hashParts[3];
+                const sFuncName = hashParts[5];
 
 
                 var newTitle = `BO `;
-                if (bofuncname) {
-                    newTitle += `${bofuncname}`;
+                if (sFuncName) {
+                    newTitle += `${sFuncName} `;
                 }
-                if (boname) {
-                    newTitle += `|${boname} `;
+                if (sName) {
+                    newTitle += `${sName} `;
                 }
-                newTitle += `|${instance}`;
+                newTitle += `${instance}`;
                 // Send a message to content script to change the title
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else if (url.hash.startsWith('#/Connector')) {
+                var sName = hashParts[3]
+                const sFuncName = hashParts[6];
+
+
+                var newTitle = `CON `;
+                if (sFuncName) {
+                    newTitle += `${sFuncName} `;
+                }
+                if (sName) {
+                    newTitle += `${sName} `;
+                }
+                newTitle += `${instance}`;
+                // Send a message to content script to change the title
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else if(url.hash.startsWith('#/ApplicationEditor')){
+                var sName = hashParts[2]
+
+                var newTitle = `APP `;
+                if (sName) {
+                    newTitle += `${sName} `;
+                }
+                newTitle += `${instance}`;
+                // Send a message to content script to change the title
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else if(url.hash.startsWith('#/Project/ProjectDetails')){
+                var sName = hashParts[4]
+
+                var newTitle = `PRO `;
+                if (sName) {
+                    newTitle += `${sName} `;
+                }
+                newTitle += `${instance}`;
+                // Send a message to content script to change the title
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else if(url.hash.startsWith('#/DbDesigner/SchemaDetails')){
+                var sName = hashParts[4]
+
+                var newTitle = `DB `;
+                if (sName) {
+                    newTitle += `${sName} `;
+                }
+                newTitle += `${instance}`;
+                // Send a message to content script to change the title
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else if(url.hash.startsWith('#/Logging/Monitoring')){
+                var newTitle = `Monitoring `;
+                newTitle += `${instance}`;
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else if(url.hash.startsWith('#/Logging/AuditLog')){
+                var newTitle = `Logs `;
+                newTitle += `${instance}`;
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else if(url.hash.startsWith('#/Logging/Statistic')){
+                var newTitle = `Stats `;
+                newTitle += `${instance}`;
+                chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
+            }else{
+                newTitle = `${instance}`;
                 chrome.tabs.sendMessage(tabId, { action: "changeTitle", newTitle: newTitle });
             }
         }
